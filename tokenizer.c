bbff4489079ceaf12ce5e373b9a4448ee65434d4
Gerb-life <92326137+Gerb-life@users.noreply.github.com>
2022-03-28 08:09:30 -0400

    80% done with project need to print to file

--- tokenizer.c
+++ tokenizer.c
@@ -11,18 +11,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "tokenizer.h"
 
 // global variables
 char *line;             // Global pointer to line of input
 
 // (optional) can declare some additional variable if you want to
-
+char token[TSIZE];
+int count = 0;
+int ind = 0;
+int statement = 1;
 /**
 * add comment
 */
 int main(int argc, char* argv[]) {
-   char  token[TSIZE];      /* Spot to hold a token, fixed size */
    char  input_line[LINE];  /* Line of input, fixed size        */
    FILE  *in_file = NULL;        /* File pointer                     */
    FILE  *out_file = NULL;
@@ -48,14 +51,30 @@ int main(int argc, char* argv[]) {
   }
 
    // (optional) can add some code here if you want some here
- 
-   while (fgets(input_line, LINE, in_file) != NULL)
-   {
-      line = input_line;  // Sets a global pointer to the memory location
-                           // where the input line resides.
+  while (fgets(input_line, LINE, in_file) != NULL){
+        line = input_line;  // Sets a global pointer to the memory location
 
-      // Add code here.
 
+     for(int i = 0 ; i < strlen(line); i++){
+                if(isspace(line[i])){
+
+                }
+
+                else if(isdigit(line[i]) == 1){
+
+                        line[i] = line[i] + '0';
+                        get_chars(line[i]);
+                }
+                else{
+                        get_chars(line[i]);
+                }
+        }
+
+
+   }
+
+   for(int i = 0  ; i < strlen(token); i++){
+        get_token(token[i]);
    }
 
    fclose(in_file);
@@ -63,10 +82,87 @@ int main(int argc, char* argv[]) {
    return 0;
 }
 
+
+void get_chars(char c){
+        token[ind] = c;
+        //printf("%c" , token[ind]);
+        ind++;
+}
 /**
-* add javadoc-like comment
+*
 */
-void get_token(char *token_ptr)
-{
-   // Add code here.
+void get_token(char token_ptr){
+        // token_ptr is the token at index i;
+        if(count == 0 ){
+                printf("Statement # %d \n" , statement);
+        }
+
+        switch (token_ptr){
+
+
+                case '0'... '9':
+                           printf("Lexeme %d is %c \n" ,count ,  token_ptr);
+                           count++;
+                           break;
+
+                case '(':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count++;
+                           break;
+
+                case ')':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count++;
+                           break;
+                case '+':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count++;
+                           break;
+
+                case '-':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count++;
+                           break;
+
+                case '*':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count++;
+                           break;
+
+                case '/':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count++;
+                           break;
+
+                case '^':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count++;
+                           break;
+                case '=':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count++;
+                           break;
+
+                case '<':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count++;
+                           break;
+                case '>':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count++;
+                           break;
+ 		case '!':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count++;
+                           break;
+                case ';':
+                           printf("Lexeme %d is %c \n", count , token_ptr);
+                           count = 0;
+                           statement++;
+                           printf("---------------------------- \n");
+                           break;
+                default:
+                           printf("Lexical error %c is not a lexeme \n" , token_ptr);
+                           break;
+        }
 }
