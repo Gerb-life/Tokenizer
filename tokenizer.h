0dfae78d55013e41511df88ebb3f5997328b1084
Gerb-life <92326137+Gerb-life@users.noreply.github.com>
2022-03-28 08:10:45 -0400

    Add files via upload

--- tokenizer.h
+++ tokenizer.h
@@ -1,17 +1,16 @@
 /**
- * Header file for the tokenizer project 
+ * Header file for the tokenizer project
  * @author your_name
  * @version date
  */
 /* Constants */
 #define LINE 100
-#define TSIZE 20
+#define TSIZE 100
 #define TRUE 1
 #define FALSE 0
 
 /**
 * add comment
 */
-void get_token(char *);   
-
-
+void get_token(char);
+void get_chars(char);
